0c751332e2f91912006f1e3d2658dd7c4a875258
uiye0n <107168009+uirong@users.noreply.github.com>
2024-04-09 14:24:59 +0900

    circleChild에서 상속 후 사각형 넓이 구하기에서 원주율 구하기 코드 추가

--- JAVA/Inheritance/FigureReactangle.java
+++ JAVA/Inheritance/FigureReactangle.java
@@ -9,8 +9,15 @@ public class FigureReactangle {
 		Ractangle r=new Ractangle(4,5);
 
 		System.out.println(r.getArea());
-//		int square =r.getArea();
-//		System.out.println(square);
+		int square =r.getArea();
+		System.out.println(square);
+
+
+//------------ circleChild 클래스에서 원주율 구하는 것 상속해서 원주율 구하기 ------------
+		
+		CircleChild cc=new CircleChild(5,30); // 반지름 : 5, 각도 : 30도
+		// 패키지가 다르니 import
+		System.out.println(cc.getArea2());
 		
 
 	}
