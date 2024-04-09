ebd8cc64a1d7bf5c3f1b000fe9e7be740341e280
uirong <107168009+uirong@users.noreply.github.com>
2024-04-09 11:55:53 +0900

    Create FigureReactangle.java

---
+++ Inheritance /FigureReactangle.java
@@ -0,0 +1,15 @@
+package Inheritance;
+
+import circle.Ractangle;
+
+public class FigureReactangle {
+
+	public static void main(String[] args) {
+		
+		Ractangle r=new Ractangle(4,5);
+		
+		int square =r.getArea();
+		System.out.println(square);
+	}
+
+}
