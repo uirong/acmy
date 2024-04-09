c09af75ed8f57f6beff4e891b2e0236deb1f20b5
uirong <107168009+uirong@users.noreply.github.com>
2024-04-09 12:13:51 +0900

    System.out.println(r.getArea()); 추가

--- Inheritance /FigureReactangle.java
+++ Inheritance /FigureReactangle.java
@@ -7,9 +7,10 @@ public class FigureReactangle {
 	public static void main(String[] args) {
 		
 		Ractangle r=new Ractangle(4,5);
+		System.out.println(r.getArea());
 		
-		int square =r.getArea();
-		System.out.println(square);
+//		int square =r.getArea();
+//		System.out.println(square);
 	}
 
 }
