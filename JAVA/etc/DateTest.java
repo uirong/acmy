e63b6b6ec2f9c7a6f40e62054d5f9f99adec63c5
uiye0n <107168009+uirong@users.noreply.github.com>
2024-04-15 12:27:02 +0900

    a날짜와 b날짜 사이의 초 구하기

--- JAVA/etc/DateTest.java
+++ JAVA/etc/DateTest.java
@@ -35,18 +35,46 @@ public class DateTest {
 				
 		*/
 		
-		Calendar cal=Calendar.getInstance(); // calendar 생성
-		cal.set(2005, 7,31);				 // 필요하면 내가 원하는 날짜로 세팅 할 수 있다 .   3 썼다고 3월이 아니라 2월인거임 ...
+		Calendar cal=Calendar.getInstance();     // calendar 생성
+		cal.set(2005, 7,31);		    	 // 필요하면 내가 원하는 날짜로 세팅 할 수 있다 .   3 썼다고 3월이 아니라 2월인거임 ...
 		System.out.println(toString(cal)); 	 // 2005년 08월 31일
 
-		cal.add(Calendar.DATE, 1); 			 // 음수는 지정 날짜에서 그만큼 빼고 양수는 그만큼 더하기
+		cal.add(Calendar.DATE, 1); 		 // 음수는 지정 날짜에서 그만큼 빼고 양수는 그만큼 더하기
 		System.out.println(toString(cal)); 	 // 2005년  9월 1일
 		cal.add(Calendar.MONTH, -6);
 		System.out.println(toString(cal)); 	 // 2005년 3월 1일 
 		cal.roll(Calendar.DATE, 31);		 // 해당 기준 값은 "일" 인데 roll은 날짜가지고 더하기 빼기 하는데 월은 바뀌지 않는다
 		System.out.println(toString(cal)); 
-		cal.add(Calendar.DATE, 31);		 	 // roll 과는 같은데 "월" 에도 영향을 준다 
+		cal.add(Calendar.DATE, 31);	 	 // roll 과는 같은데 "월" 에도 영향을 준다 
 		System.out.println(toString(cal)); 	 // 2005년 4월 1일
+
+
+
+		final int[] TIME_UNIT = { 3600, 60, 1 };
+		final String[] TIME_UNIT_MATE = { "시간","분", "초" };
+
+		Calendar time1 = Calendar.getInstance();
+		Calendar time2 = Calendar.getInstance();
+
+		time1.set(Calendar.HOUR_OF_DAY, 10);
+		time1.set(Calendar.MINUTE, 20);
+		time1.set(Calendar.SECOND, 30); // 10시 20분 30초
+
+		time2.set(Calendar.HOUR_OF_DAY, 20);
+		time2.set(Calendar.MINUTE, 30);
+		time2.set(Calendar.SECOND, 10); // 10시 30분 10초
+
+		long difference = Math.abs(time2.getTimeInMillis() - time1.getTimeInMillis()) / 1000;
+		System.out.println("time1과 time2의 차이는 " + difference + "초 입니다");
+		 
+		String tmp="";
+		for (int i = 0; i < TIME_UNIT.length; i++) {
+			tmp += difference / TIME_UNIT[i] + TIME_UNIT_MATE[i];
+			difference %= TIME_UNIT[i];  // 시간 정보만큼 버린 만큼의 나머지 날짜
+		 }
+		System.out.println("시분초의 차이: "+tmp);
+		
+		
 	}// main
 	public static String toString(Calendar cal) {
 		return cal.get(Calendar.YEAR)+"년"+(cal.get(Calendar.MONTH)+1)+"월"+(cal.get(Calendar.DATE))+"일";
