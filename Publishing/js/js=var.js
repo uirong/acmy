48d0846606b39fb953a29944718c04724a4e5e06
uiye0n <107168009+uirong@users.noreply.github.com>
2024-04-23 15:59:51 +0900

    js start

---
+++ Publishing/js/js=var.js
@@ -0,0 +1,19 @@
+
+// !  문자, 숫자, 논리, 객체, undifinded(변수에 값이 없을 때)
+
+var x;
+x=10;
+console.log(x); // *콘솔에 찍기
+document.write(x+"<br>"); // *html 화면에 찍기
+
+var x,y,z;
+x=10; y=20; z=30;
+
+z=100;
+document.write(typeof(x+"<br>"));
+
+x="hong"; // ? == 'hong';
+document.write(typeof(x+"<br>"));
+
+var x;
+x=23.8;
---
+++ Publishing/js/jsTest.js
@@ -0,0 +1,3 @@
+function messesge(){
+    alert('messege');
+}
