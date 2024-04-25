1b3eb9a6b0343e6357b72e0b0d98a50c067a1140
uiye0n <107168009+uirong@users.noreply.github.com>
2024-04-25 14:07:40 +0900

    jsWinP.js 수정

--- Publishing/js/jsWinP.js
+++ Publishing/js/jsWinP.js
@@ -1,10 +1,10 @@
 
 // window.open("jsWinC.html","pop1","width=300px,height=400px,left=300px,top=50px")
 
-function newWin(){ // ? 원하는 부분에 onclick="newWin()" 해줘야 적용 
-    window.open("jsWinC.html","","width=400,height=300,top=50,left=50");
+function newWin() { // ? 원하는 부분에 onclick="newWin()" 해줘야 적용 
+    window.open("jsWinC.html", "", "width = 400px, height = 300px, top= 50px, left = 50px"); //? ""창이름 비워두됨
 }
 
 function sendVal(){
-    document("aaa");
+    
 }
