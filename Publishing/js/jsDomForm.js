c8c63a17b8c60c7bc614d95a4976b9173f107917
uiye0n <107168009+uirong@users.noreply.github.com>
2024-04-29 18:37:32 +0900

    gender 변경

--- Publishing/js/jsDomForm.js
+++ Publishing/js/jsDomForm.js
@@ -6,7 +6,7 @@ function memChk(frm){
     let checkOut=document.querySelectorAll('.chk:checked').length;
     //? 클래스가 .chk라고 된 상태가 checkbox에 checked한 상태만 가져온 개수를 가져온다
     //? 체크가 최소 1개 이상의 체크가 되어야하고 카운트가 1 이상부터 시작한다
-    let checkGen=document.querySelectorAll('.chk:checked').length;
+    // let checkGen=document.querySelectorAll('.chk:checked').length;
 
     
     // frm.id.value
@@ -37,8 +37,8 @@ function memChk(frm){
         frm.email.focus();
         return false;   
     }
-    if(checkGen==0){
-    // if(frm.gender.value==""){
+    //if(checkGen==0){
+     if(frm.gender.value==""){
         alert("성별을 입력하세요");
         return false;
     }
