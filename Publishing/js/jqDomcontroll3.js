b26941cf2e71f70f52940c29cc9cb7f59bf612a0
uiye0n <107168009+uirong@users.noreply.github.com>
2024-05-02 12:24:44 +0900

    요소의 위치조작 추가

--- Publishing/js/jqDomcontroll3.js
+++ Publishing/js/jqDomcontroll3.js
@@ -12,4 +12,18 @@ $(function(){
     console.log(w3); // 100
 
     $("#p2").outerWidth(100).outerHeight(100);
+
+    // ~요소의 위치조작~
+    //! position() :  상대 위치값
+    //! offset().[left,top] : 절대 위치값
+
+    //* form- val /텍스트- text
+    let $txt1=$(".txt_1 span"),
+        $txt2=$(".txt_2 span"),
+        $box=$(".box");
+    let off_t = $box.offset().top; //?  100  브라우저기준
+    let pos_t = $box.position().top; //? 50  부모기준, 현재 자신기준
+
+    $txt1.text(off_t);
+    $txt1.text(pos_t);
 });
